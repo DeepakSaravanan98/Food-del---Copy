bd84029214d34c681491c4344fda297a31a440a0
DeepakSaravanan98 <sdeepakvlr@gmail.com>
2025-01-06 12:42:57 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './components/navbar/navbar'
-import Sidebar from './components/sidebar/sidebar'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 import {Route, Routes} from 'react-router-dom'
 import Add from './pages/add/Add'
 import List from './pages/list/List'
